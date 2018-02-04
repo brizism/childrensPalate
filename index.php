3b8e497010c90139651c3e8276bd5e99e54958ae
brizism <montielbriseida@gmail.com>
2018-02-03 22:47:28 -0500

    add html includes

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("_inc/head.html"); ?>
+<title>Children's Palate | Fighting Childhood Obesity</title>
+</head>
+<body>
+  <?php
+    include("_inc/header.html");
+  ?>
+  
+  
+<script src="js/index.js"></script>
+</body>
+</html>
